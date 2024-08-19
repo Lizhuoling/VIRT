dfc3c8fc5209e1c98d467e18ded16f9a90296772
Li Zhuoling <1105069197@qq.com>
2024-08-19 18:55:47 +0800

    structure_change

--- scripts/sim_data.sh
+++
@@ -1,4 +0,0 @@
-xvfb-run -a python3 record_sim_episodes.py \
-    --task_name sim_insertion_scripted \
-    --dataset_dir /home/cvte/twilight/home/data/aloha/sim_insertion_scripted \
-    --num_episodes 50
--- utils/samplers/__init__.py
+++
@@ -1,9 +0,0 @@
-from .grouped_batch_sampler import GroupedBatchSampler
-from .distributed_sampler import (
-    TrainingSampler,
-    InferenceSampler,
-)
-
-__all__ = ["GroupedBatchSampler",
-           "TrainingSampler",
-           "InferenceSampler",]
