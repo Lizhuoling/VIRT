f3bd2e6031e987cecf1bbf2c44a92a67a60eb5a0
Li Zhuoling <1105069197@qq.com>
2024-09-06 20:40:01 +0800

    2024.9.6-sub3

--- act/scripts/train.sh
+++ act/scripts/train.sh
@@ -1,13 +1,13 @@
-exp_id=debug
+exp_id=aloha_openlid
 config_name=aloha_openlid
 
 nnodes=1
 nproc_per_node=1
 node_rank=0
 master_addr=127.0.0.1
-master_port=29513
+master_port=29515
 
-CUDA_VISIBLE_DEVICES=0 torchrun --nnodes=$nnodes --nproc_per_node=$nproc_per_node --node_rank=$node_rank --master_addr=$master_addr --master_port $master_port \
+CUDA_VISIBLE_DEVICES=1 torchrun --nnodes=$nnodes --nproc_per_node=$nproc_per_node --node_rank=$node_rank --master_addr=$master_addr --master_port $master_port \
     imitate_episodes.py \
     --config_name $config_name \
     --save_dir ./outputs/$exp_id \
